e11dc9b75a1c6ef1a651ea920214fa61655d52d9
Rushil210 <Rushpatel@valdosta.edu>
2026-02-13 12:49:45 -0500

    Add new enemy features and hopefully fix Git issues

--- game/systems/enemies/Enemy.gd
+++ game/systems/enemies/Enemy.gd
@@ -6,12 +6,12 @@ var health := max_health
 var direction := Vector2.LEFT
 
 
+func _ready():
+	$AnimationPlayer.play("idle")
+	
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
+func attack():
+	$AnimationPlayer.play("attack")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
